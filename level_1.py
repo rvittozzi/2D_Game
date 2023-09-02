e74bf8d05dfb021b66acd67909913c98616fa9db
rvittozzi <137367134+rvittozzi@users.noreply.github.com>
2023-09-02 14:15:33 -0400

    respawning rates made

--- level_1.py
+++ level_1.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-import start_screen
+from start_screen import start_screen  # Import the start_screen function
 
 # Initialize Pygame
 pygame.init()
@@ -8,14 +8,14 @@ pygame.init()
 # Define constants
 WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
 GAME_STATE_START, GAME_STATE_PLAY = 1, 2
-PUNCH_DAMAGE, PUNCH_HITBOX_WIDTH, PUNCH_HITBOX_HEIGHT, PUNCH_COOLDOWN = 20, 20, 40, 30
+PUNCH_DAMAGE, PUNCH_HITBOX_WIDTH, PUNCH_HITBOX_HEIGHT, PUNCH_COOLDOWN = 25, 20, 40, 60
 animationSpeed = 0.2
 
 # Create the game window
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("2D Fighter")
 
-# Load individual sprite images
+# Load individual sprite images and other assets
 idle = pygame.image.load("sprites/Player1/idle/idle.png")
 run = pygame.image.load("sprites/Player1/run/run.png")
 background = pygame.image.load("sprites/background_assets/background.png")
@@ -38,13 +38,13 @@ player1 = {
     "currentFrame": 1,
     "animationTimer": 2,
     "idleFrame": 1,
-    "scaled_width": 50 * scaling_factor,  # Scaled width
-    "scaled_height": 100 * scaling_factor,  # Scaled height
-    "is_jumping": False,  # Flag to track if the player is jumping
-    "jump_height": 10,  # Adjust this value to control jump height
-    "jump_velocity": 0,  # Vertical velocity during jump
-    "is_punching": False,  # Flag to track if the player is punching
-    "punch_cooldown": 0,  # Cooldown timer for punches
+    "scaled_width": 50 * scaling_factor,
+    "scaled_height": 100 * scaling_factor,
+    "is_jumping": False,
+    "jump_height": 10,
+    "jump_velocity": 0,
+    "is_punching": False,
+    "punch_cooldown": 0,
 }
 
 # Define enemy variables
@@ -57,9 +57,9 @@ enemy = {
     "health": 100,
     "hits": 0,
     "punchCooldown": 0,
-    "facing_left": False,  # Flag to track enemy's facing direction
-    "scaled_width": 50 * scaling_factor,  # Scaled width
-    "scaled_height": 100 * scaling_factor,  # Scaled height
+    "facing_left": False,
+    "scaled_width": 50 * scaling_factor,
+    "scaled_height": 100 * scaling_factor,
 }
 
 # Game state
@@ -67,31 +67,40 @@ gameState = GAME_STATE_START
 
 # Define the draw_health_bar function
 def draw_health_bar(surface, x, y, current_health, max_health, width=100, height=10):
-    # Calculate the width of the health bar based on current health
     health_width = (current_health / max_health) * width
 
-    # Determine the color based on health percentage
     if current_health > 0.6 * max_health:
-        color = (0, 255, 0)  # Green
+        color = (0, 255, 0)
     elif current_health > 0.3 * max_health:
-        color = (255, 255, 0)  # Yellow
+        color = (255, 255, 0)
     else:
-        color = (255, 0, 0)  # Red
+        color = (255, 0, 0)
 
-    # Draw the background of the health bar
     pygame.draw.rect(surface, (255, 0, 0), (x, y, width, height))
-
-    # Draw the filled part of the health bar
     pygame.draw.rect(surface, color, (x, y, health_width, height))
 
 # Load individual sprite images for idle and run
-idle_frames = [pygame.image.load(f"sprites/Player1/idle/idle{i}.png") for i in range(1, 5)]  # Assuming there are 4 idle frames
-run_frames = [pygame.image.load(f"sprites/Player1/run/run{i}.png") for i in range(1, 5)]  # Assuming there are 4 run frames
-current_animation = idle_frames  # Start with idle animation
-animation_index = 0  # Index to keep track of the current frame
+idle_frames = [pygame.image.load(f"sprites/Player1/idle/idle{i}.png") for i in range(1, 5)]
+run_frames = [pygame.image.load(f"sprites/Player1/run/run{i}.png") for i in range(1, 5)]
+current_animation = idle_frames
+animation_index = 0
 
 # Create a boolean variable to track whether the player is moving
 is_moving = False
+
+# Define ground variables
+ground_width = WINDOW_WIDTH
+ground_height = 100
+ground_color = (100, 100, 100)
+
+# Create a ground surface
+ground_surface = pygame.Surface((ground_width, ground_height))
+ground_surface.fill(ground_color)
+
+# Define enemy respawn variables and constants
+enemy_respawn_timer = 0
+enemy_respawn_delay = 5  # 5 seconds respawn delay
+enemy_respawned = False
 
 # Main game loop
 clock = pygame.time.Clock()
@@ -104,17 +113,14 @@ while True:
             pygame.quit()
             sys.exit()
 
-    screen.fill((0, 0, 0))  # Clear the screen with black
+    screen.fill((0, 0, 0))
 
     if gameState == GAME_STATE_START:
-        # Scale the background image to fit the window size
         scaled_background = pygame.transform.scale(background, (WINDOW_WIDTH, WINDOW_HEIGHT))
-
-        # Display the start screen
-        start_screen.start_screen(screen, WINDOW_WIDTH, WINDOW_HEIGHT)
-
-        # Draw the start screen background
         screen.blit(scaled_background, (0, 0))
+
+        # Use the start_screen function to display the start screen
+        start_screen(screen, WINDOW_WIDTH, WINDOW_HEIGHT)
 
         if keys[pygame.K_SPACE]:
             gameState = GAME_STATE_PLAY
@@ -123,15 +129,14 @@ while True:
     right_boundary = WINDOW_WIDTH - player1["width"]
 
     if gameState == GAME_STATE_PLAY:
-        screen.blit(scaled_background, (0, 0))  # Draw the background
+        screen.blit(ground_surface, (0, WINDOW_HEIGHT - ground_height))
+        screen.blit(scaled_background, (0, 0))
 
-        # Check for movement keys (left or right)
         if keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]:
             is_moving = True
         else:
             is_moving = False
 
-        # Handle player input
         if keys[pygame.K_LEFT]:
             player1["x"] -= player1["speed"] * animationSpeed * 0.1
             if player1["x"] < left_boundary:
@@ -142,54 +147,58 @@ while True:
             if player1["x"] > right_boundary:
                 player1["x"] = right_boundary
 
-        # Check if the player is on the ground (not jumping)
         if not player1["is_jumping"]:
-            # Handle jumping
             if keys[pygame.K_SPACE]:
                 player1["is_jumping"] = True
-                player1["jump_velocity"] = -player1["jump_height"]  # Negative velocity for upward motion
+                player1["jump_velocity"] = -player1["jump_height"]
 
         else:
-            # Apply gravity to the player
-            player1["jump_velocity"] += 0.5  # Adjust this value for gravity strength
+            player1["jump_velocity"] += 0.5
             player1["y"] += player1["jump_velocity"]
 
-            # Check if the player has landed on the ground
-            if player1["y"] >= 300:  # Adjust this value based on your ground level
+            if player1["y"] >= 300:
                 player1["y"] = 300
                 player1["is_jumping"] = False
                 player1["jump_velocity"] = 0
 
-        # Handle punching
         if keys[pygame.K_v] and player1["punch_cooldown"] <= 0:
             player1["is_punching"] = True
             player1["punch_cooldown"] = PUNCH_COOLDOWN
 
-        # Decrease punch cooldown timer
         if player1["punch_cooldown"] > 0:
             player1["punch_cooldown"] -= 1
 
-        # Animate the player
         if player1["is_punching"]:
-            current_animation = idle_frames  # Switch to idle animation when punching
-        elif is_moving:
-            # Alternate between idle and run animation frames
-            animation_index = (animation_index + 1) % len(run_frames)
-            current_animation = run_frames
+            current_animation = idle_frames
+            if (
+                player1["x"] + player1["scaled_width"] >= enemy["x"]
+                and player1["x"] <= enemy["x"] + enemy["scaled_width"]
+                and player1["y"] + player1["scaled_height"] >= enemy["y"]
+                and player1["y"] <= enemy["y"] + enemy["scaled_height"]
+            ):
+                enemy["health"] -= PUNCH_DAMAGE
+                if enemy["health"] <= 0:
+                    enemy["x"] = -enemy["scaled_width"]
+                    enemy["y"] = -enemy["scaled_height"]
+                    enemy_respawn_timer = pygame.time.get_ticks() + enemy_respawn_delay * 1000
+                    enemy_respawned = False
         else:
-            current_animation = idle_frames  # Default to idle animation when not moving
+            if is_moving:
+                animation_index = (animation_index + 1) % len(run_frames)
+                current_animation = run_frames
+            else:
+                current_animation = idle_frames
 
-        # Render the player character with scaled size
+        if not enemy_respawned and pygame.time.get_ticks() >= enemy_respawn_timer:
+            enemy["x"] = 600
+            enemy["y"] = 300
+            enemy["health"] = 100
+            enemy_respawned = True
+
         screen.blit(pygame.transform.scale(current_animation[animation_index], (player1["scaled_width"], player1["scaled_height"])), (player1["x"], player1["y"]))
-
-        # Render the enemy character with scaled size
         screen.blit(pygame.transform.scale(enemy_sprite, (enemy["scaled_width"], enemy["scaled_height"])), (enemy["x"], enemy["y"]))
-
-        # Draw player's health bar
-        draw_health_bar(screen, player1["x"], player1["y"] - 20, player1["health"], 100)  # Adjust the position as needed
-
-        # Draw enemy's health bar
-        draw_health_bar(screen, enemy["x"], enemy["y"] - 20, enemy["health"], 100)  # Adjust the position as needed
+        draw_health_bar(screen, player1["x"], player1["y"] - 20, player1["health"], 100)
+        draw_health_bar(screen, enemy["x"], enemy["y"] - 20, enemy["health"], 100)
 
     pygame.display.flip()
-    clock.tick(60)  # Reduce the tick rate for a slower animation
+    clock.tick(60)
